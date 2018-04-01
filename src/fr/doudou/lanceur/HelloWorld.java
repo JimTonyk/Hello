e0d62c3b84528d45f7086a0aac4ecdc754e44c85
Jean-Michel Hiltbrunner <jm.hiltbrunner@outlook.com>
2018-04-01 18:44:16 +0200

    Ajout d'une ligne à la classe Hello

--- src/fr/doudou/lanceur/HelloWorld.java
+++ src/fr/doudou/lanceur/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-system.out.println("Encore un texte!");
+		System.out.println("Encore un texte!");
 
 	}
 
