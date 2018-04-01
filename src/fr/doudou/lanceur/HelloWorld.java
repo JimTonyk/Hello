9d19ed2ddfa5b6292718751b0f16db72b02c345e
Jean-Michel Hiltbrunner <jm.hiltbrunner@outlook.com>
2018-04-01 18:41:07 +0200

    Ajout de la classe Hello

---
+++ src/fr/doudou/lanceur/HelloWorld.java
@@ -0,0 +1,11 @@
+package fr.doudou.lanceur;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+system.out.println("Encore un texte!");
+
+	}
+
+}
